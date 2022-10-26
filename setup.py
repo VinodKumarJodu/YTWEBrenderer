58ab48480a667e0c31a218030141f84cf8de5e8f
VinodKumarJodu <vinodkumarjodu@gmail.com>
2022-10-26 06:37:14 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+from typing import List
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -8,6 +9,22 @@ __version__ = "0.0.0"
 REPO_NAME = "YTWEBrenderer"
 AUTHOR_NAME = "VinodKumarJodu"
 SRC_REPO = "YTWEBrenderer"
+
+REQUIREMENTS_FILE_NAME = "requirements_dev.txt"
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements() -> List[str]:
+    """
+    This Function will return the list of requirements listed in requirements.txt
+    """
+    requirements_list: List[str]= []
+    with open(REQUIREMENTS_FILE_NAME,'r') as f:
+        # contents = csv.reader(f)
+        # requirements_list = [line[0] for line in contents]
+        if HYPHEN_E_DOT in requirements_list:
+            requirements_list.remove(HYPHEN_E_DOT)
+        print(requirements_list)
+    return requirements_list
 
 setup(
     name=SRC_REPO,
@@ -21,5 +38,6 @@ setup(
         "Bug Tracker": f"https://github.com/{AUTHOR_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
+    packages=find_packages(),
+    install_requires = get_requirements()
 )
