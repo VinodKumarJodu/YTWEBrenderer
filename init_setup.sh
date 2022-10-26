28fe1815c567431acf064dfbe222e7332a1edc93
VinodKumarJodu <vinodkumarjodu@gmail.com>
2022-10-26 07:25:31 +0530

    Updated the files

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,10 @@
 echo [$(date)]: "Start"
 echo [$(date)]: "Creating Conda Environment with python 3.8"
-conda create --prefix ./venv python=3.8 -y
-echo [$(date)]: "Activating the Created Environment"
-source activate ./venv
+conda create -p ./env python=3.8 -y
+echo [$(date)]: "Activating the Created Conda Environment"
+source activate ./env
 echo [$(date)]: "Installing dev requirements"
-pip install -r requirements.txt
+pip install -r requirements_dev.txt
 echo [$(date)]: "End"
+
+
